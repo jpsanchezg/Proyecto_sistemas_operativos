b3611050ea8c4a270232653f22f625509641bd93
juan pablo sanchez <juansanchezg@javeriana.edu.co>
2022-04-02 20:33:04 -0500

    creando el header pipes.h

--- makefile
+++ makefile
@@ -1,3 +1,4 @@
 build:
 	@gcc -o publicador.o publicador.c
+	@gcc -o sistemadecomunicacion.o sistemadecomunicacion.c
 	@gcc -o suscriptor.o suscriptor.c
--- pipes.h
+++ pipes.h
@@ -3,3 +3,92 @@
 
 /* Inclusión de archivos */
 #include "precompiler.h"
+#define TAM_PIPES 100
+
+/* Funciones */
+/**************************************************************************
+*   Función:   limpiarTerminal()
+*   Proposito:  Rutina para limpiar el terminal
+*   Argumentos: 
+*       void
+*   Retorno:
+*       void
+**************************************************************************/
+void limpiarTerminal()
+{
+    printf("\033[2J");
+    printf("\033[H");
+}
+
+/**************************************************************************
+*   Función:   abrirPipe()
+*   Proposito:  Rutina para abrir el pipe que tiene el nombre pasado
+*               por parametro, en caso de no estar creado se pone a 
+*               esperar a que sea abierto.
+*   Argumentos:
+*       nombrePipe: Cadena de caracteres que tiene el nombre del pipe
+*                   que se va a abrir.
+*   Retorno:
+*       int: Se retorna descriptor del pipe que es abierto.
+**************************************************************************/
+int abrirPipe(char nombrePipe[TAM_STRING])
+{
+    int fd;
+    int counter = 0;
+    int tamAnim = strlen(ANIM_CARGA);
+    limpiarTerminal();
+    do
+    {
+        fd = open(nombrePipe, O_WRONLY | O_NONBLOCK);
+        if (fd == -1)
+        {
+            printf("\rEsperando creacion del pipe %c", ANIM_CARGA[counter]);
+            fflush(stdout);
+            counter++;
+            counter %= tamAnim;
+            sleep(1);
+            printf("\r                                \r");
+            fflush(stdout);
+        }
+    } while (fd == -1);
+    limpiarTerminal();
+    //printf("Se ha abierto el pipe %s\n", nombrePipe);
+    sleep(1);
+    return fd;
+} // fin abrirPipe
+
+/**************************************************************************
+*   Función:   crearPipe()
+*   Proposito:  Rutina para crear y abrir el pipe que tiene el nombre
+*                pasado por parametro.
+*   Argumentos:
+*       nombrePipe: Cadena de caracteres que tiene el nombre del pipe
+*                   que se va a abrir.
+*   Retorno:
+*       int: Se retorna descriptor del pipe que es creado y abierto.
+**************************************************************************/
+int crearPipe(char nombrePipe[])
+{
+    int fd;
+    unlink(nombrePipe);
+    mkfifo(nombrePipe, 0);
+    chmod(nombrePipe, 460);
+    fd = open(nombrePipe, O_RDONLY);
+    return fd;
+} // fin crearPipe
+
+/**************************************************************************
+*   Función:   leerPipe()
+*   Proposito:  Rutina para leer el pipe que se pasa por parametro.
+*   Argumentos:
+*       fd: Descriptor del pipe que se va a leer.
+*       mensaje: Cadena de caracteres donde se almacenara lo leído.
+*   Retorno:
+*       void
+**************************************************************************/
+void leerPipe(int fd, char mensaje[TAM_STRING])
+{
+    while (read(fd, mensaje, TAM_STRING) == 0);
+} // fin leerPipe
+
+#endif
