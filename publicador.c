45e31c43b8649800137ca4a477269edaf030445f
juan pablo sanchez <juansanchezg@javeriana.edu.co>
2022-04-04 20:10:27 -0500

    creando publicador

--- publicador.c
+++ publicador.c
@@ -18,12 +18,33 @@ struct subscripcion
     struct Noticia *noticias;
 };
 
-int main(int arg, char *args[])
-{
-    
-}
 
 void publicar(struct Noticia *noticia, struct Pipe *pipe)
 {
     write(pipe->fd, noticia, sizeof(struct Noticia));
 }
+
+int main(int argc, char *args[])
+{
+    banderaArchivoSalida = 0;
+    if (atenderArgumentosReceptor(argc, args))
+    {
+        if (leerBaseDeDatos())
+        {
+            atexit(salidaReceptor);
+            printf("=========================================================================\n");
+            printf("              Proceso receptor\n");
+            printf("=========================================================================\n");
+            printf("               r. Generar reporte\n");
+            printf("               s. Salir\n");
+            printf("=========================================================================\n");
+            pthread_t hiloMenu;
+            pthread_create(&hiloMenu, NULL, atenderEntradaConsola, NULL);
+            inicializarPipes();
+            pipeReceptor = crearPipe(nombrePipeReceptor);
+            atenderPipeReceptor();
+            pthread_join(hiloMenu, NULL);
+        }
+    }
+    return 0;
+} // fin main
--- sistemadecomunicacion.c
+++ sistemadecomunicacion.c
@@ -23,3 +23,27 @@ void imprimirsuscripciones(struct subscripcion *subscripcion, struct Pipe *pipe)
     read(pipe->fd, subscripcion, sizeof(struct subscripcion));
 }
 //
+int main(int argc, char *args[])
+{
+    banderaArchivoSalida = 0;
+    if (atenderArgumentosReceptor(argc, args))
+    {
+        if (leerBaseDeDatos())
+        {
+            atexit(salidaReceptor);
+            printf("=========================================================================\n");
+            printf("              Proceso receptor\n");
+            printf("=========================================================================\n");
+            printf("               r. Generar reporte\n");
+            printf("               s. Salir\n");
+            printf("=========================================================================\n");
+            pthread_t hiloMenu;
+            pthread_create(&hiloMenu, NULL, atenderEntradaConsola, NULL);
+            inicializarPipes();
+            pipeReceptor = crearPipe(nombrePipeReceptor);
+            atenderPipeReceptor();
+            pthread_join(hiloMenu, NULL);
+        }
+    }
+    return 0;
+} // fin main
